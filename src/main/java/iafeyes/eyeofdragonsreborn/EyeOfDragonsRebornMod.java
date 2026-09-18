e2c3a2bc87cc7c5c70f27b4573305689003b22dd
Arakiel <arakiel0sakura@gmail.com>
2026-09-18 21:55:31 +0800

    v1.4.1

--- src/main/java/iafeyes/eyeofdragonsreborn/EyeOfDragonsRebornMod.java
+++ src/main/java/iafeyes/eyeofdragonsreborn/EyeOfDragonsRebornMod.java
@@ -10,7 +10,12 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.DeferredRegister;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Mod(EyeOfDragonsRebornMod.MODID)
 public class EyeOfDragonsRebornMod {
@@ -41,7 +46,17 @@ public class EyeOfDragonsRebornMod {
         ItemInit.ITEMS.register(bus);
         CREATIVE_TABS.register(bus);
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, EyeOfDragonsRebornConfig.SPEC);
+        Path modConfigDir = FMLPaths.CONFIGDIR.get().resolve(MODID);
+        try {
+            Files.createDirectories(modConfigDir);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create config directory for " + MODID, e);
+        }
+
+        ModLoadingContext.get().registerConfig(
+                ModConfig.Type.COMMON,
+                EyeOfDragonsRebornConfig.SPEC,
+                MODID + "/" + MODID + "-common.toml");
 
         MinecraftForge.EVENT_BUS.register(this);
     }
